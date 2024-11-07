fbcaf370dae6fb0055db090e12f7decc5f4e00cd
zebra76966 <rs288442@gmail.com>
2024-11-07 23:15:57 +0530

    added webmail redirect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useNavigate } from "react-router-dom";
 import Home from "./components/main";
 import Header from "./components/header";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if the current URL path is '/webmail'
+    if (window.location.pathname === "/webmail") {
+      // Redirect to the specified URL
+      window.location.href = "https://premium90.web-hosting.com:2096/cpsess7010092364/3rdparty/roundcube/?_task=mail&_mbox=INBOX";
+    }
+  }, [window.location.pathname]); // Run the effect when the path changes
   return (
     <>
       <Header />
